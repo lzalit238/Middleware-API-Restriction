a9446da4e27cc832a21aeb4adc26ce1897972d02
lzalit238 <lzalit238@west-mec.org>
2021-11-17 15:22:40 -0700

    Create Project
    npm install nodemon, express, and morgan
    create authorizedUser.js and main.js

---
+++ authorizedUser.js
@@ -0,0 +1,25 @@
+const kite = (req,res,next) => {
+    const method = req.method
+    const url = req.url
+    const thing = 'this is a kite'
+    console.log(method, url, thing)
+    next()
+}
+
+const authorizedUser = (req, res, next) => {
+
+    const { apikey } = req.query
+    if (apikey === '')
+    {    console.log('authorized')
+    //modifies the request object for the next response
+        req.user = { email: apikey, id: 1 }
+        next()
+    }
+    else {
+        console.log('unauthorized access')
+        res.send({result:[], status: 401, message: 'unauthorized access: use email as api key'})
+    }
+}
+
+module.exports = kite
+module.exports = authorizedUser
---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const morgan = require('morgan')
+const authorizedUser = require('./authorizedUser')
+const kite = require('./authorizedUser')
+const app = express()
+
+app.use([kite, authorizedUser])
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home</h1>')
+})
+
+app.get('/api', (req, res) => {
+    console.log(req.user)
+    res.send('at the end of the string there is a kite')
+})
+
+app.listen(5000, () => {
+    console.log('server is listening on port 5000...')
+})
